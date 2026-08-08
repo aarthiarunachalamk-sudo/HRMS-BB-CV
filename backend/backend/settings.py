38bca7169076299ec560efcb0329ecfe66b6e49b
AARTHI <aarthikarthikeyana@gmail.com>
2026-08-08 16:58:22 +0530

    recent commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,6 +101,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # First la irukkanum bro!
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -219,6 +220,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
  
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
