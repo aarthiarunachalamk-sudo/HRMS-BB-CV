30fc3cd8dd5a9c78416e75d66a9335256e1668d5
AARTHI <aarthikarthikeyana@gmail.com>
2026-09-08 16:10:30 +0530

    gradle

--- frontend/android/build.gradle.kts
+++ frontend/android/build.gradle.kts
@@ -1,15 +1,5 @@
 import org.gradle.api.tasks.compile.JavaCompile
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
-    }
-}
-
 allprojects {
     repositories {
         google()
