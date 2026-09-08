547ce80765783c4ecb68f3a0779fb167bc8521a8
AARTHI <aarthikarthikeyana@gmail.com>
2026-09-08 11:23:16 +0530

    new changes

--- backend/backend/local_settings.py
+++
@@ -1,9 +0,0 @@
-from .settings import *  # noqa: F403
-
-# Use a file-backed SQLite DB for local development so migrations persist
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
--- backend/hrms/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class HrmsConfig(AppConfig):
-    name = 'hrms'
-
-    def ready(self):
-        # Register additive employee-account provisioning for leadership users.
-        from . import signals  # noqa: F401
